3a5b4499279c18b3692fb8c0c0d6d58f2a6648da
Feddy <feddy_7990@hotmail.com>
2018-10-16 22:57:12 -0300

    Codigo Incial

---
+++ PokeFarm/PokeFarm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PokeFarm
+{
+    partial class PokeFarm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pokemonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pokemonToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.apiLoader = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.pokemonToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItem_Click);
+            // 
+            // pokemonToolStripMenuItem
+            // 
+            this.pokemonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actualizarToolStripMenuItem});
+            this.pokemonToolStripMenuItem.Name = "pokemonToolStripMenuItem";
+            this.pokemonToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.pokemonToolStripMenuItem.Text = "API";
+            // 
+            // actualizarToolStripMenuItem
+            // 
+            this.actualizarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pokemonToolStripMenuItem1});
+            this.actualizarToolStripMenuItem.Name = "actualizarToolStripMenuItem";
+            this.actualizarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.actualizarToolStripMenuItem.Text = "Actualizar";
+            // 
+            // pokemonToolStripMenuItem1
+            // 
+            this.pokemonToolStripMenuItem1.Name = "pokemonToolStripMenuItem1";
+            this.pokemonToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.pokemonToolStripMenuItem1.Text = "Pokemon";
+            this.pokemonToolStripMenuItem1.Click += new System.EventHandler(this.PokemonToolStripMenuItem1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 17);
+            this.statusLabel.Text = "Listo.";
+            // 
+            // apiLoader
+            // 
+            this.apiLoader.DoWork += new System.ComponentModel.DoWorkEventHandler(this.apiLoader_DoWork);
+            // 
+            // PokeFarm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PokeFarm";
+            this.Text = "PokeFarm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pokemonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pokemonToolStripMenuItem1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.ComponentModel.BackgroundWorker apiLoader;
+    }
+}
+
---
+++ PokeFarm/PokeFarm.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using PokeFarm.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokeFarm
+{
+    public partial class PokeFarm : Form
+    {
+        private const string baseURL = "https://pokeapi.co/api/v2/";
+
+        public PokeFarm()
+        {
+            InitializeComponent();
+        }
+
+        private PokemonInfo LoadJson()
+        {
+            using (StreamReader r = new StreamReader(@"JSON/PokemonSrc.json"))
+            {
+                string json = r.ReadToEnd();
+                PokemonInfo pkmInfo = JsonConvert.DeserializeObject<PokemonInfo>(json);
+                /*foreach (var pkm in pkmInfo.results)
+                {
+                    MessageBox.Show("Pokemon: " + pkm.name);
+                }*/
+                return pkmInfo;
+            }
+        }
+
+        private void DownloadJSON(string path, string fileName)
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.DownloadFile(baseURL + path.ToLower(), Directory.GetCurrentDirectory() + @"\json\" + fileName + ".json");
+            }
+        }
+
+        private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PokemonToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DownloadJSON("pokemon/", "PokemonSrc");
+            PokemonInfo pkmInfo = LoadJson();
+
+            foreach (var pkm in pkmInfo.results)
+            {
+                DownloadJSON("pokemon/" + pkm.name, pkm.name);
+                statusLabel.Text = "Descargado " + pkm.name;
+            }
+
+            MessageBox.Show("Descarga Completa");
+        }
+
+        private void apiLoader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Pasarle los parametros para que lo haga aca
+        }
+    }
+}
